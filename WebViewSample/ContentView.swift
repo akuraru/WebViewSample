e7a864206592e49b4a42154ae7432ff56964e305
akuraru <akuraru@gmail.com>
2024-12-01 23:12:13 +0900

    Initial Commit

---
+++ WebViewSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebViewSample
+//
+//  Created by 金原有志 on 2024/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebViewSample/WebViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewSampleApp.swift
+//  WebViewSample
+//
+//  Created by 金原有志 on 2024/12/01.
+//
+
+import SwiftUI
+
+@main
+struct WebViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
